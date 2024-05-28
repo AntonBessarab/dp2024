b801a0910e3674b8086f82e1f56c71a4cd65dbf1
AntonBessarab <antonbessarab55@gmail.com>
2024-05-28 16:16:31 +0300

    complete

--- Lab1/Lab6/src/main/java/com/example/Lab6/repository/MyRepository.java
+++ Lab1/Lab6/src/main/java/com/example/Lab6/repository/MyRepository.java
@@ -2,9 +2,9 @@ package com.example.Lab6.repository;
 
 import com.example.Lab6.entity.MyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "myEntities", path = "myEntities")
 public interface MyRepository extends JpaRepository<MyEntity, Long> {
-
 }
